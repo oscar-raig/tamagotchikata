8707e976705e407e24d15ad5a158ab87bc501718
Oscar Raig <oscar.raig@scmspain.com>
2015-08-10 09:35:02 +0200

    Isolating try/catch

--- src/main/java/org/raig/Main.java
+++ src/main/java/org/raig/Main.java
@@ -17,9 +17,7 @@ public final class Main {
     public static void main(String[] argv) {
         logger.debug("Indian Pale Ale is the best beer you have tasted!");
 
-
         ClockTamagotchi clock = new ClockTamagotchi(ClockTamagotchi.DEFAULT_MILLISECONDS_PERIOD);
-
 
         Tamagotchi tamagotchi =  new Tamagotchi(INITIAL_HAPPINESS);
         clock.addObserver(tamagotchi);
@@ -27,19 +25,23 @@ public final class Main {
         new Thread(clock).start();
         logger.debug("Init happiness :" + tamagotchi.getHappiness());
 
+        
         tamagotchi.feed();
         logger.debug("After feeding the tamagotchi happiness has been Increased " + tamagotchi.getHappiness());
 
+        sleeping();
+        logger.debug("After Sleeping more " + SLEEP_TIME + " seconds happiness has decreased : " + tamagotchi.getHappiness());
+
+        clock.die();
+    }
+
+    private static void sleeping() {
         try {
             sleep(SLEEP_TIME);
         } catch (InterruptedException e) {
             logger.error("Error Sleeping!!");
             return;
         }
-
-        logger.debug("After Sleeping more " + SLEEP_TIME + " seconds happiness has decreased : " + tamagotchi.getHappiness());
-
-        clock.die();
     }
 
 }
